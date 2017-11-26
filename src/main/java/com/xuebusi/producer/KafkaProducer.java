f8d5c52bd685eae38124656527da81c40289b13a
xuebusi <xbs1019@126.com>
2017-11-26 16:25:48 +0800

    优化

--- src/main/java/com/xuebusi/producer/KafkaProducer.java
+++ src/main/java/com/xuebusi/producer/KafkaProducer.java
@@ -24,29 +24,9 @@ public class KafkaProducer {
      * 定时任务1
      */
     @Scheduled(cron = "00/1 * * * * ?")
-    public void send1(){
+    public void send(){
         String message = UUID.randomUUID().toString();
-        ListenableFuture future = kafkaTemplate.send("app_log_1", message);
-        future.addCallback(o -> System.out.println("send1-消息发送成功：" + message), throwable -> System.out.println("消息发送失败：" + message));
-    }
-
-    /**
-     * 定时任务2
-     */
-    @Scheduled(cron = "00/1 * * * * ?")
-    public void send2(){
-        String message = UUID.randomUUID().toString();
-        ListenableFuture future = kafkaTemplate.send("app_log_2", message);
-        future.addCallback(o -> System.out.println("send2-消息发送成功：" + message), throwable -> System.out.println("消息发送失败：" + message));
-    }
-
-    /**
-     * 定时任务3
-     */
-    @Scheduled(cron = "00/1 * * * * ?")
-    public void send3(){
-        String message = UUID.randomUUID().toString();
-        ListenableFuture future = kafkaTemplate.send("app_log_3", message);
-        future.addCallback(o -> System.out.println("send3-消息发送成功：" + message), throwable -> System.out.println("消息发送失败：" + message));
+        ListenableFuture future = kafkaTemplate.send("app_log", message);
+        future.addCallback(o -> System.out.println("消息发送成功：" + message), throwable -> System.out.println("消息发送失败：" + message));
     }
 }
